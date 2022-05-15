3593668e3a3592d27ad4f69269728f6ab4eb172c
Sairam Chekuri <sairamchekuri@Sairams-MacBook-Pro.local>
2022-05-15 12:29:26 +0530

    Initial Commit

---
+++ SpringScheduler/src/test/java/com/sairam/SpringScheduler/SpringSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sairam.SpringScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
