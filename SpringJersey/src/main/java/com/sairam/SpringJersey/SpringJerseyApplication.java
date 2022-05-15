6ae09d398306df59471731c8570fc5d48c28a5fd
sairamchekuri <sairamc3@gmail.com>
2022-05-15 15:20:48 +0530

    Initial running application

---
+++ SpringJersey/src/main/java/com/sairam/SpringJersey/SpringJerseyApplication.java
@@ -0,0 +1,13 @@
+package com.sairam.SpringJersey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJerseyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJerseyApplication.class, args);
+	}
+
+}
---
+++ SpringJersey/src/test/java/com/sairam/SpringJersey/SpringJerseyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sairam.SpringJersey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJerseyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
