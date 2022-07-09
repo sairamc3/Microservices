0b870498e9f5338e916037b30947322bc1428b60
sairamchekuri <sairamc3@gmail.com>
2022-07-09 19:05:08 +0530

    Initial Commit of Lodge Service - Spring

---
+++ vennela/src/main/java/com/lodge/vennela/VennelaApplication.java
@@ -0,0 +1,13 @@
+package com.lodge.vennela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VennelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VennelaApplication.class, args);
+	}
+
+}
